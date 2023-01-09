94172d54393cc0e149060fdcbc9cedc47e8bdb68
james-2142 <104553422+james-2142@users.noreply.github.com>
2023-01-09 12:22:43 +1100

    style & formatting updates

--- genkml_func.py
+++ genkml_func.py
@@ -3,78 +3,78 @@ import fnmatch
 from osgeo import gdal
 from osgeo import osr
 
-def genkml(ipath,tgt_epsg):
-	topo, ext = os.path.splitext(os.path.basename(ipath))
 
-	ver = gdal.__version__
-	maj_ver = int(ver.split(".")[0])
+def genkml(ipath, tgt_epsg):
+    topo, ext = os.path.splitext(os.path.basename(ipath))
 
-	fh = open(ipath + os.sep + "doc.kml",'w')
-	fh.write("""<?xml version="1.0" encoding="iso-8859-1"?>
+    ver = gdal.__version__
+    maj_ver = int(ver.split(".")[0])
+
+    fh = open(ipath + os.sep + "doc.kml", 'w')
+    fh.write("""<?xml version="1.0" encoding="iso-8859-1"?>
 <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
 <Document>
 <name>%s</name>"""
-	% topo)
+             % topo)
 
-	for filename in fnmatch.filter( os.listdir(ipath), '*.tif'):
+    for filename in fnmatch.filter(os.listdir(ipath), '*.tif'):
 
-		name, ext = os.path.splitext(os.path.basename(filename))
+        name, ext = os.path.splitext(os.path.basename(filename))
 
-# Thanks to James for the post on Stackexchange for this transform which is licensed under CC BY-SA 2.5 for which the following is modified from.
-# https://gis.stackexchange.com/a/201320
+        # Thanks to James for the post on Stackexchange for this transform which is
+        # licensed under CC BY-SA 2.5for which the following is modified from.
+        # https://gis.stackexchange.com/a/201320
 
-		src = gdal.Open(ipath + os.sep + filename)
-		xp = src.RasterXSize
-		yp = src.RasterYSize
-		proj = src.GetProjection()
+        src = gdal.Open(ipath + os.sep + filename)
+        xp = src.RasterXSize
+        yp = src.RasterYSize
+        proj = src.GetProjection()
 
-		ulx, xsize, _, uly, _, ysize  = src.GetGeoTransform()
-		lrx = ulx + xp*xsize
-		lry = uly + yp*ysize
+        ulx, xsize, _, uly, _, ysize = src.GetGeoTransform()
+        lrx = ulx + xp * xsize
+        lry = uly + yp * ysize
 
-		src = None
+        src_ref = osr.SpatialReference()
+        src_ref.ImportFromWkt(proj)
 
-		src_ref = osr.SpatialReference()
-		src_ref.ImportFromWkt(proj)
+        tgt_ref = osr.SpatialReference()
+        if maj_ver >= 3:
+            tgt_ref.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
+        tgt_ref.ImportFromEPSG(tgt_epsg)
 
-		tgt_ref = osr.SpatialReference()
-		if maj_ver >= 3:
-			tgt_ref.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
-		tgt_ref.ImportFromEPSG(tgt_epsg)
+        coord_transform = osr.CoordinateTransformation(src_ref, tgt_ref)
+        w, n, _ = coord_transform.TransformPoint(ulx, uly)
+        e, s, _ = coord_transform.TransformPoint(lrx, lry)
 
-		coord_transform = osr.CoordinateTransformation(src_ref, tgt_ref)
-		w, n, _ = coord_transform.TransformPoint(ulx, uly)
-		e, s, _ = coord_transform.TransformPoint(lrx, lry)
-
-		fh.write("""
+        fh.write("""
 <Folder>
 <name>%s</name>"""
-		% name)
+                 % name)
 
-		fh.write("""
+        fh.write("""
 <GroundOverlay>
 <drawOrder>%d</drawOrder>"""
-		% 1)
+                 % 1)
 
-		fh.write("""
+        fh.write("""
 <Icon>
 <href>files/%s.jpg</href>"""
-		% name)
+                 % name)
 
-		fh.write("""
+        fh.write("""
 </Icon>
 <LatLonBox>
 <north>%.15g</north>
 <south>%.15g</south>
 <east>%.15g</east>
 <west>%.15g</west>"""
-		%( n, s, e, w ) )
+                 % (n, s, e, w))
 
-		fh.write("""
+        fh.write("""
 </LatLonBox>
 </GroundOverlay>
-</Folder>""" )
-		
-	fh.write("""
+</Folder>""")
+
+    fh.write("""
 </Document>
 </kml>""")
