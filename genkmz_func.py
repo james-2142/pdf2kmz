97b064ad0c1c461f9a942f0baf01e3d331b9d258
james-2142 <104553422+james-2142@users.noreply.github.com>
2023-01-09 11:37:49 +1100

    style & formatting updates

--- genkmz_func.py
+++ genkmz_func.py
@@ -1,19 +1,21 @@
-import zipfile, os
+import os
+import zipfile
 
-def genkmz(of,ipath):
-	if1 = os.path.join(ipath,'doc.kml')
-	if2 = os.path.join(ipath,'files')
 
-	zip = zipfile.ZipFile(of, "w")
+def genkmz(of, ipath):
+    if1 = os.path.join(ipath, 'doc.kml')
+    if2 = os.path.join(ipath, 'files')
 
-	arcname = os.path.basename(if1)
-	dname = os.path.dirname(if1)
-	zip.write(if1, arcname )
+    zip = zipfile.ZipFile(of, "w")
 
-	for filename in os.listdir(if2):
-		fname = os.path.join(if2,filename)
-		pname = os.path.relpath(if2,dname)
-		arcname = os.path.join(pname,filename)
-		zip.write(fname, arcname)
-		
-	zip.close()
+    arcname = os.path.basename(if1)
+    dname = os.path.dirname(if1)
+    zip.write(if1, arcname)
+
+    for filename in os.listdir(if2):
+        fname = os.path.join(if2, filename)
+        pname = os.path.relpath(if2, dname)
+        arcname = os.path.join(pname, filename)
+        zip.write(fname, arcname)
+
+    zip.close()
